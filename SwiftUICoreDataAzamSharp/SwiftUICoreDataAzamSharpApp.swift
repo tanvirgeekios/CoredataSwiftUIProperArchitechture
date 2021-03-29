ee426bf231cb1aafd197eacd5e40188631eaae0f
tanvirgeekios <tanvir.alam@iosventure.com>
2021-03-29 09:38:20 +0600

    Initial Commit

---
+++ SwiftUICoreDataAzamSharp/SwiftUICoreDataAzamSharpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoreDataAzamSharpApp.swift
+//  SwiftUICoreDataAzamSharp
+//
+//  Created by MD Tanvir Alam on 29/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoreDataAzamSharpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
